01a861db1974f141d25fc473a9b8825774161705
3ngineer <aiman.mahrat@ryerson.ca>
2023-01-31 17:18:04 -0500

    Added navbar skeleton and images

--- index.php
+++ index.php
@@ -5,23 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- normalize css -->
     <link rel="stylesheet" href="./normalize.css">
-    <link rel="stylesheet" type="text/css" href="./styles.css">
-    <title>Room Climate</title>
+    <link rel="stylesheet" href="./styles.css">
+    <title>IEQ</title>
   </head>
   <body>
+    <!-- nav -->
+    <nav class="navbar">
+      <div class="nav-center">
+        <!-- header -->
+        <div class="nav-header">
+          <a href="index.php" class="nav-logo">
+            <img src="./assets/" alt="IEQ">
+          </a>
+          <button type="button" class="btn nav-btn">
+            <i class="fas fa-align-justify"></i>
+          </button> 
+        </div>
+        <!-- links -->
+        <div class="nav-links">
+          <a href="index.php" class="nav-link">home</a>
+          <a href="Graphs.html" class="nav-link">graphs</a>
+          <a href="tags.html" class="nav-link">settings</a>
+        </div>
+      </div>
+    </nav>
+    <!-- end of nav -->
+
     <div class="container">
       <div class="climate-data">
         <div class="temperature">
-          <h2>Temperature</h2>
-          <div id="temperature_value"></div>
+          <img src="./assets/temperature100.png" alt="thermometer" class="temperature_img">
+          <div>
+            <h2>Temperature</h2>
+            <div class="temperature_value" id="temperature_value"></div>
+          </div>
         </div>
         <div class="pressure">
-          <h2>Pressure</h2>
-          <div id="pressure_value"></div>
+          <img src="./assets/pressure100.png" alt="barometer" class="pressure_img">
+          <div>
+            <h2>Pressure</h2>
+            <div class="pressure_value" id="pressure_value"></div>
+          </div>
         </div>
         <div class="humidity">
-          <h2>Humidity</h2>
-          <div id="humidity_value"></div>
+          <img src="./assets/humidity100.png" alt="drop" class="humidity_img">
+          <div>
+            <h2>Humidity</h2>
+            <div class="humidity_value" id="humidity_value"></div>
+          </div>
         </div>
       </div>
     </div>
@@ -43,9 +74,9 @@
       });
   }
   window.addEventListener('load', function () {
-  // Your document is loaded.
-  var fetchInterval = 2000; // 2 seconds.
-
-  setInterval(fetchData, fetchInterval);
-});
+    // Your document is loaded.
+    fetchData();
+    var fetchInterval = 2000; // 2 seconds.
+    setInterval(fetchData, fetchInterval);
+  });
 </script>
