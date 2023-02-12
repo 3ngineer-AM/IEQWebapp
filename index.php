7e71618d2844d7ed09fcdb6bc053785469e9c05b
3ngineer <aiman.mahrat@ryerson.ca>
2023-02-12 12:36:23 -0500

    Added color change and Dynamic Rating bar

--- index.php
+++ index.php
@@ -41,6 +41,7 @@
               <h1>Rating</h1>
               <div class="progress-container">
                 <div class="progress-bar"></div>
+                <div class="progress-value" id="temp-rating"></div>
               </div>
             </div>
             <div class="card-info">
@@ -62,6 +63,7 @@
               <h1>Rating</h1>
               <div class="progress-container">
                 <div class="progress-bar"></div>
+                <div class="progress-value" id="press-rating"></div>
               </div>
             </div>
             <div class="card-info">
@@ -83,6 +85,7 @@
               <h1>Rating</h1>
               <div class="progress-container">
                 <div class="progress-bar"></div>
+                <div class="progress-value" id="humid-rating"></div>
               </div>
             </div> 
             <div class="card-info">
@@ -106,7 +109,7 @@
 <script>
   function fetchData() {
   // Retrieving json array from query.php
-  fetch("http://10.0.0.200:8080/query.php") 
+    fetch("http://10.0.0.200:8080/query.php") 
       .then((res) => res.json())
       .then((data) => {
           const temperature = document.getElementById("temperature_value");
@@ -115,6 +118,41 @@
           pressure.textContent = data.pressure_value + " Pa";
           const humidity = document.getElementById("humidity_value");
           humidity.textContent = data.humid_value + " %";
+
+          var temp_rating = Math.round(10 - (Math.abs(data.temp_value-19))); /*Function for finding rating of the temperature */
+          if (temp_rating < 0) temp_rating = 0;
+          const temp_rating_value = document.getElementById("temp-rating");
+          temp_rating_value.textContent = temp_rating + "/10";
+          document.documentElement.style /*Sets Css variable to the rating value so it can show in progress bar */
+            .setProperty('--temp_rating', String(temp_rating));
+          
+          var temp_rating = Math.round(10 - (Math.abs(data.temp_value-19))); /*Function for finding rating of the pressure */
+          if (temp_rating < 0) temp_rating = 0;
+          const press_rating_value = document.getElementById("press-rating");
+          press_rating_value.textContent = temp_rating + "/10";
+          document.documentElement.style /*Sets Css variable to the rating value so it can show in progress bar */
+            .setProperty('--press_rating', String(temp_rating));
+          
+          var temp_rating = Math.round(10 - (Math.abs(data.temp_value-19))); /*Function for finding rating of the humidity */
+          if (temp_rating < 0) temp_rating = 0;
+          const humid_rating_value = document.getElementById("humid-rating");
+          humid_rating_value.textContent = temp_rating + "/10";
+          document.documentElement.style /*Sets Css variable to the rating value so it can show in progress bar */
+            .setProperty('--humid_rating', String(temp_rating));
+
+          if (temp_rating < 0) temp_rating = 0;
+          if (temp_rating >= 5 && temp_rating < 8) {
+            document.documentElement.style /*Sets Css bar color depending on rating */
+              .setProperty('--bar-color', '#FFB833');
+          }
+          else if (temp_rating >= 8){
+            document.documentElement.style 
+              .setProperty('--bar-color', '#247F36');
+          }
+          else {
+            document.documentElement.style 
+              .setProperty('--bar-color', '#AD1F2D');
+          }
       });
   }
   window.addEventListener('load', function () {
@@ -123,4 +161,5 @@
     var fetchInterval = 2000; // 2 seconds.
     setInterval(fetchData, fetchInterval);
   });
+
 </script>
