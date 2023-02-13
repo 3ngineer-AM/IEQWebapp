b4f21195654ed25b2a2595fc3167e307094f8ed9
3ngineer <aiman.mahrat@ryerson.ca>
2023-02-13 02:11:56 -0500

    Moved Javascript to different file, added more mobile support, started graphs section

---
+++ graphs.php
@@ -0,0 +1,40 @@
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Javascript -->
+        <script src="fetch.js"></script>
+        <!-- normalize css -->
+        <link rel="stylesheet" href="./normalize.css">
+        <link rel="stylesheet" href="./styles.css">
+        <script src="https://kit.fontawesome.com/e14c9d9e40.js" crossorigin="anonymous"></script>
+        <title>Graphs</title>
+    </head>
+    <body>
+      <!-- nav -->
+      <nav class="navbar">
+        <div class="nav-center">
+            <!-- links -->
+            <div class="nav-links">
+                <button class="link-buttons" onclick="window.location.href ='index.php';">
+                  <i class="fa-solid fa-house"></i>
+                </button>
+                <a href="index.php" class="nav-link">home</a>
+                <button class="link-buttons" onclick="window.location.href ='graphs.php';">
+                  <i class="fa-solid fa-chart-line"></i>
+                </button>
+                <a href="graphs.php" class="nav-link">graphs</a>
+                <button class="link-buttons" onclick="window.location.href ='index.php';">
+                  <i class="fa-solid fa-gear"></i>
+                </button>
+                <a href="index.php" class="nav-link">settings</a>
+            </div>
+        </div>
+      </nav>
+      <!-- end of nav -->
+
+      
+    </body>
+</html>
--- index.php
+++ index.php
@@ -14,20 +14,31 @@
     <nav class="navbar">
       <div class="nav-center">
         <!-- header -->
+        <div class="nav-header">
+          <button type="button" class="btn nav-btn">
+            <i class="fas fa-align-justify"></i>
+          </button> 
+        </div>
         <!-- links -->
         <div class="nav-links">
-          <button class="link-buttons">
-            <i class="fa-solid fa-house"></i>
-          </button>
-          <a href="index.php" class="nav-link">home</a>
-          <button class="link-buttons">
-            <i class="fa-solid fa-chart-line"></i>
-          </button>
-          <a href="Graphs.html" class="nav-link">graphs</a>
-          <button class="link-buttons">
-            <i class="fa-solid fa-gear"></i>
-          </button>
-          <a href="tags.html" class="nav-link">settings</a>
+          <div class="links">
+            <button class="link-buttons" onclick="window.location.href ='index.php';">
+              <i class="fa-solid fa-house"></i>
+            </button>
+            <a href="index.php" class="nav-link">home</a>
+          </div>
+          <div class="links">
+            <button class="link-buttons" onclick="window.location.href ='graphs.php';">
+              <i class="fa-solid fa-chart-line"></i>
+            </button>
+            <a href="graphs.php" class="nav-link">graphs</a>
+          </div>
+          <div class="links">
+            <button class="link-buttons" onclick="window.location.href ='index.php';">
+              <i class="fa-solid fa-gear"></i>
+            </button>
+            <a href="index.php" class="nav-link">settings</a>
+          </div>
         </div>
       </div>
     </nav>
@@ -103,63 +114,8 @@
         </div>
       </div>
     </div>
+    <!-- Javascript -->
+    <script src="fetch.js"></script>
+    <script src="navLinks.js"></script>
   </body>
 </html>
-
-<script>
-  function fetchData() {
-  // Retrieving json array from query.php
-    fetch("http://10.0.0.200:8080/query.php") 
-      .then((res) => res.json())
-      .then((data) => {
-          const temperature = document.getElementById("temperature_value");
-          temperature.textContent = data.temp_value + " °C";
-          const pressure = document.getElementById("pressure_value");
-          pressure.textContent = data.pressure_value + " Pa";
-          const humidity = document.getElementById("humidity_value");
-          humidity.textContent = data.humid_value + " %";
-
-          var temp_rating = Math.round(10 - (Math.abs(data.temp_value-19))); /*Function for finding rating of the temperature */
-          if (temp_rating < 0) temp_rating = 0;
-          const temp_rating_value = document.getElementById("temp-rating");
-          temp_rating_value.textContent = temp_rating + "/10";
-          document.documentElement.style /*Sets Css variable to the rating value so it can show in progress bar */
-            .setProperty('--temp_rating', String(temp_rating));
-          
-          var temp_rating = Math.round(10 - (Math.abs(data.temp_value-19))); /*Function for finding rating of the pressure */
-          if (temp_rating < 0) temp_rating = 0;
-          const press_rating_value = document.getElementById("press-rating");
-          press_rating_value.textContent = temp_rating + "/10";
-          document.documentElement.style /*Sets Css variable to the rating value so it can show in progress bar */
-            .setProperty('--press_rating', String(temp_rating));
-          
-          var temp_rating = Math.round(10 - (Math.abs(data.temp_value-19))); /*Function for finding rating of the humidity */
-          if (temp_rating < 0) temp_rating = 0;
-          const humid_rating_value = document.getElementById("humid-rating");
-          humid_rating_value.textContent = temp_rating + "/10";
-          document.documentElement.style /*Sets Css variable to the rating value so it can show in progress bar */
-            .setProperty('--humid_rating', String(temp_rating));
-
-          if (temp_rating < 0) temp_rating = 0;
-          if (temp_rating >= 5 && temp_rating < 8) {
-            document.documentElement.style /*Sets Css bar color depending on rating */
-              .setProperty('--bar-color', '#FFB833');
-          }
-          else if (temp_rating >= 8){
-            document.documentElement.style 
-              .setProperty('--bar-color', '#247F36');
-          }
-          else {
-            document.documentElement.style 
-              .setProperty('--bar-color', '#AD1F2D');
-          }
-      });
-  }
-  window.addEventListener('load', function () {
-    // Your document is loaded.
-    fetchData();
-    var fetchInterval = 2000; // 2 seconds.
-    setInterval(fetchData, fetchInterval);
-  });
-
-</script>
